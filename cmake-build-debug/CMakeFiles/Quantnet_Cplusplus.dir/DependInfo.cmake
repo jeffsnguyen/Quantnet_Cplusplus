849c4551c40ed215287fc9d7b1fac0d1b19d5232
jeffsnguyen <son.jeff.nguyen@gmail.com>
2021-05-18 22:28:01 -0500

    create first lecture files

---
+++ cmake-build-debug/CMakeFiles/Quantnet_Cplusplus.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/jeffsnguyen/OneDrive/Cloud Storage/Courses/Quantnet/C++/Quantnet_Cplusplus/Level_1/Lecture/1_1/Hello.cpp" "/Users/jeffsnguyen/OneDrive/Cloud Storage/Courses/Quantnet/C++/Quantnet_Cplusplus/cmake-build-debug/CMakeFiles/Quantnet_Cplusplus.dir/Level_1/Lecture/1_1/Hello.cpp.o"
+  "/Users/jeffsnguyen/OneDrive/Cloud Storage/Courses/Quantnet/C++/Quantnet_Cplusplus/Level_1/Lecture/1_1/TestHello.cpp" "/Users/jeffsnguyen/OneDrive/Cloud Storage/Courses/Quantnet/C++/Quantnet_Cplusplus/cmake-build-debug/CMakeFiles/Quantnet_Cplusplus.dir/Level_1/Lecture/1_1/TestHello.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../Level_1/Lecture/1_1"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Quantnet_Cplusplus.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Quantnet_Cplusplus.dir/Level_1/Lecture/1_1/Hello.cpp.o"
+  "CMakeFiles/Quantnet_Cplusplus.dir/Level_1/Lecture/1_1/TestHello.cpp.o"
+  "Quantnet_Cplusplus"
+  "Quantnet_Cplusplus.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Quantnet_Cplusplus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
