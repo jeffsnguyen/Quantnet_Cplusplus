91dcb11c01638651a9c41301ac2fa1a577043f15
jeffsnguyen <son.jeff.nguyen@gmail.com>
2021-05-23 14:16:04 -0500

    Exercise 1.7.2

--- Level_2/Homework/Section_1_7/Exercise_2/test_Length.c
+++ Level_2/Homework/Section_1_7/Exercise_2/test_Length.c
@@ -2,37 +2,59 @@
 // Level: 1
 // Section: 1.7 Pointers and Arrays
 // Exercise: 2
-// Description: Main file to test Swap()
-// Try to create a function Swap().
-//  This function must exchange the value of two variables.
-//  For example: if i=123 and j=456, then i=456 and j=123 after the Swap() function has been called.
-//  The variables i and j are declared, initialised and printed in the function main().
-//  This problem can be solved by using pointers as arguments for the Swap()function.
+// Description: Main file to test Length()
+// The following program reads a string with a 30 character maximum.
+//  Implement the Length() function.
+//  The function Length() must determine the length of the string.
+//  Give Length() the address of the array as argument.
+//  Note: your Length() function should be similar to the built-in strlen()
+//      function so your job is to mimic that function without using it.
+//
+//  EOF is used in the function main().
+//  This means End-of-File and is discussed later on in this document.
+//  In DOS, EOF can be entered by the key combination Ctrl-z (often written as ^Z).
+//  With ^Z (Say: control Z) is meant pressing the control-key and the z-key simultaneously.
 
 /*---------------------------------*/
+/* Calculate the length of a string */
 #include <stdio.h>
-#include "Defs.h"
+#define MAXLINE 30
+// String length declaration
+int Length(char str[]);
 
 int main()
 {
-    // Declare parameters i,j
-    int i,j;
+    char string[MAXLINE+1]; // Line of maxium 30 chars + \0
+    int c; // The input character
+    int i=0; // The counter
 
-    // Input parameters x,y,z
-    printf("Input an integer value of i = ");
-    scanf("%d", &i);
+    // Print intro text
+    printf("Type up to %d chars. Exit with ^Z\n", MAXLINE);
 
-    printf("Input an integer value of j = ");
-    scanf("%d", &j);
+    // Get the characters
+    while ((c=getchar())!=EOF && i<MAXLINE)
+    {
+        // Append entered character to string
+        string[i++]=(char)c;
+    }
 
-    printf("Pre-swap (i,j) = (%d,%d)\n", i,j);  // Display values of vars pre-swap
-
-    Swap(&i,&j);  // Running the Swap function
-    printf("Post-swap (i,j) = (%d,%d)\n", i,j);  // Display values of vars post-swap
-
+    string[i]='\0';  // String must be closed with \0
+    printf("String length is %d\n", Length(string));
 
     return 0;
 }
 
+/*---------------------------------*/
+/* Implement the Length() function here */
+int Length(char str[])
+{
+    int idx = 0;  // Declaring and initializing a string index variable
 
+    // Loop through each string index until '\0'
+    while (str[idx] != '\0')
+    {
+        idx++;
+    };
 
+    return idx;
+}
