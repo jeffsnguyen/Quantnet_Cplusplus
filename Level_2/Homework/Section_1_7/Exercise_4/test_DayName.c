6b4f4f2e64ed29d1bde34f13a46c8f51ac29882b
jeffsnguyen <son.jeff.nguyen@gmail.com>
2021-05-23 20:13:34 -0500

    Exercise 1.7.4

---
+++ Level_2/Homework/Section_1_7/Exercise_4/test_DayName.c
@@ -0,0 +1,47 @@
+// Type: Homework
+// Level: 1
+// Section: 1.7 Pointers and Arrays
+// Exercise: 4
+// Description: Main file to test DayName() and include definition of DayName()
+// Create a C-program that has a function DayName()
+//  which can print the day of a given day-number.
+//  For example:
+//      1 gives: Day 1 is a Sunday
+//      7 gives: Day 7 is a Saturday.
+//  The day-name (1-7) should be written "hard-coded” into the program using an array of strings.
+
+/*---------------------------------*/
+#include <stdio.h>
+
+/*---------------------------------*/
+void DayName(idx)
+{
+    // Create a dayname array contains strings value of dates
+    char daynameArr[7][10]={
+                            "Sunday",
+                            "Monday",
+                            "Tuesday",
+                            "Wednesday",
+                            "Thursday",
+                            "Friday",
+                            "Saturday",
+                            };
+    printf("Name of the date index %d is %s.", idx, daynameArr[idx-1]);
+}
+
+int main()
+{
+    // Declare parameters dayIdx
+    int dayIdx;
+
+    // Input parameters dayIdx
+    printf("Input an integer value from 1->7 = ");
+    scanf("%d", &dayIdx);
+
+    DayName(dayIdx);  // Run DayName() to output corresponded text value of the day index
+
+    return 0;
+}
+
+
+
