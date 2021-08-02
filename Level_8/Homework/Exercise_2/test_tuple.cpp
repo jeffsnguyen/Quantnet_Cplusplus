d2de9c055d5ba870880c87aa37826e94549cad67
jeffsnguyen <son.jeff.nguyen@gmail.com>
2021-08-01 20:30:16 -0500

    Exercise 2, simple tuple using boost

--- Level_8/Homework/Exercise_2/test_tuple.cpp
+++ Level_8/Homework/Exercise_2/test_tuple.cpp
@@ -1,56 +1,46 @@
 // Type: Homework
 // Level: 8
 // Section: Tuple
-// Exercise: 1
+// Exercise: 2
 // Description: Test features of STL containers
-// Boost tuples can be used to store different kinds of data as one entity. It can be used if we need to combine data without the need to create a separate class.
-//In this exercise, create a typedef for a Person tuple that contains a name, age and length. Also create a function that prints the person tuple. Use the get<T>() member functions the retrieve the data. Create a few person tuple instances and print them.
-//Also increment the age of one of the persons. Note that to change a value of one of the tuple elements, you can also use the get<T>() function since it returns a reference to the value.
+// Boost tuples can be used to store different kinds of data as one entity.
+// It can be used if we need to combine data without the need to create a separate class.
+// In this exercise, create a typedef for a Person tuple that contains a name, age and length.
+// Also create a function that prints the person tuple.
+// Use the get<T>() member functions the retrieve the data.
+// Create a few person tuple instances and print them.
+// Also increment the age of one of the persons.
+// Note that to change a value of one of the tuple elements,
+//  you can also use the get<T>() function since it returns a reference to the value.
 
 /*---------------------------------*/
 
-#include "boost/shared_ptr.hpp"
+#include "boost//tuple/tuple.hpp"
+#include "boost/tuple/tuple_io.hpp"
 #include <iostream>
-#include "Array.hpp"
-#include "ArrayException.hpp"
-#include "Circle.hpp"
-#include "Line.hpp"
-#include "NumericArray.hpp"
-#include "Point.hpp"
-#include "Shape.hpp"
-
+#include <string>
 
 using namespace std;
 
-typedef boost::shared_ptr<Shape> ShapePtr;
-typedef Array<ShapePtr> ShapeArray;
+typedef boost::tuple<string, int, double> Person;
 
 /*---------------------------------*/
 int main()
 {
     /*---------------------------------*/
-    // Initialize array with 3 shapes
-    ShapeArray shapeArr(3);
+    // Initialize different persons
+    Person ppl1 = boost::make_tuple(string("Person 1"), 67, 200);
+    Person ppl2 = boost::make_tuple(string("Person 2"), 77, 20);
+    Person ppl3 = boost::make_tuple(string("Person 3"), 76, 2);
 
-    // Test creation of different shapes
-    // Point
-    shapeArr[0] = ShapePtr(new Point(1,1));
-    shapeArr[0]->Print();
+    // Display persons info
+    cout << "Name: " << ppl1.get<0>() << ", age: " << ppl1.get<1>() << ", length: " << ppl1.get<2>() << endl;
+    cout << "Name: " << ppl2.get<0>() << ", age: " << ppl2.get<1>() << ", length: " << ppl2.get<2>() << endl;
+    cout << "Name: " << ppl3.get<0>() << ", age: " << ppl3.get<1>() << ", length: " << ppl3.get<2>() << endl;
 
-    // Line
-    shapeArr[1] = ShapePtr(new Line(Point(1,1), Point(2,2)));
-    shapeArr[1]->Print();
-
-    // Circle
-    shapeArr[2] = ShapePtr(new Circle(Point(1,1), 1));
-    shapeArr[2]->Print();
-
-    /*---------------------------------*/
-    // Check reference count
-    cout << shapeArr[0].use_count() << endl;
-    cout << shapeArr[1].use_count() << endl;
-    cout << shapeArr[2].use_count() << endl;
-
+    // Increment length of person 2 and reprint
+    ppl2.get<2>() = 2000;
+    cout << "Name: " << ppl2.get<0>() << ", age: " << ppl2.get<1>() << ", length: " << ppl2.get<2>() << endl;
     return 0;
 }
 
--- cmake-build-debug/CMakeFiles/Quantnet_Cplusplus.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/Quantnet_Cplusplus.dir/cmake_clean.cmake
@@ -1,5 +1,5 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/Quantnet_Cplusplus.dir/Level_8/Homework/Exercise_1/test_smartPointers.cpp.o"
+  "CMakeFiles/Quantnet_Cplusplus.dir/Level_8/Homework/Exercise_2/test_tuple.cpp.o"
   "Quantnet_Cplusplus"
   "Quantnet_Cplusplus.pdb"
 )
